62c4c4053de5fd813237183e878bfa4ffa449346
赛博日记 <diary@cyber.local>
2026-05-23 15:36:15 +0800

    赛博日记 - 个人技能成长日记

---
+++ quartz.config.ts
@@ -0,0 +1,97 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+/**
+ * Quartz 4 Configuration
+ *
+ * See https://quartz.jzhao.xyz/configuration for more information.
+ */
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "赛博日记",
+    pageTitleSuffix: " | Cyber Diary",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "plausible",
+    },
+    locale: "zh-CN",
+    baseUrl: "localhost",
+    ignorePatterns: ["private", "templates", ".obsidian"],
+    defaultDateType: "created",
+    theme: {
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        header: "Noto Sans SC",
+        body: "Noto Sans SC",
+        code: "JetBrains Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#eef2f8",
+          lightgray: "#d0d8e3",
+          gray: "#8a9bb5",
+          darkgray: "#3d4f66",
+          dark: "#1a2a3a",
+          secondary: "#1a5c8c",
+          tertiary: "#00b8d4",
+          highlight: "rgba(26, 92, 140, 0.12)",
+          textHighlight: "#4fc3f788",
+        },
+        darkMode: {
+          light: "#070d1a",
+          lightgray: "#0f1b2e",
+          gray: "#2a3f5c",
+          darkgray: "#8ba4c4",
+          dark: "#c8d6e5",
+          secondary: "#0aefff",
+          tertiary: "#1a7acc",
+          highlight: "rgba(10, 239, 255, 0.10)",
+          textHighlight: "#0aefff66",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "git", "filesystem"],
+      }),
+      Plugin.SyntaxHighlighting({
+        theme: {
+          light: "github-light",
+          dark: "github-dark",
+        },
+        keepBackground: false,
+      }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.Favicon(),
+      Plugin.NotFoundPage(),
+      // Comment out CustomOgImages to speed up build time
+      // Plugin.CustomOgImages(),
+    ],
+  },
+}
+
+export default config
